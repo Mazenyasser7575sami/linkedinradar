c3a1590e32a1c1a1b215b2827c490437bad1a367
Mazenyasser7575sami <yassersami75@gmail.com>
2025-07-10 12:57:48 +0300

    linkedinPeopleAlsoViewed.js

--- linkedinPeopleAlsoViewed.js
+++ linkedinPeopleAlsoViewed.js
@@ -1,11 +1,13 @@
+const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 require('dotenv').config({ path: './cookies.env' });
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: chromium.args,
+    executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
+    headless: chromium.headless,
   });
 
   const page = await browser.newPage();
