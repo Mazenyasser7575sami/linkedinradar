fc322d0dd4a5c949a36e17cb38d0106160f11b5b
Mazenyasser7575sami <yassersami75@gmail.com>
2025-07-10 13:08:46 +0300

    linkedinPeopleAlsoViewed.js

--- linkedinPeopleAlsoViewed.js
+++ linkedinPeopleAlsoViewed.js
@@ -7,7 +7,8 @@ require('dotenv').config({ path: './cookies.env' });
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
+    executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
+
     headless: chromium.headless,
   });
 
